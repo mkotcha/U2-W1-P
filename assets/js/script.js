d82f374c7058469209dd81da9bfd1cf3eab04f0f
Matteo Falasca <matteo.falasca@emmek.org>
2023-07-28 17:26:23 +0200

    is better now

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,14 @@ const checkNav = event => {
 document.addEventListener("scroll", checkNav);
 
 const emme = () => {
-  const elements = document.querySelectorAll("svg > g > g > g[opacity='0']");
-  let rnd = Math.floor(Math.random() * elements.length);
-  elements[rnd].setAttribute("opacity", "1");
+  const elements0 = document.querySelectorAll("svg > g > g > g[opacity='0']");
+  const elements1 = document.querySelectorAll("svg > g > g > g[opacity='1']");
 
-  elements = document.querySelectorAll("svg > g > g > g[opacity='1']");
-  rnd = Math.floor(Math.random() * elements.length);
-  elements[rnd].setAttribute("opacity", "0");
+  let rnd = Math.floor(Math.random() * elements0.length);
+  elements0[rnd].setAttribute("opacity", "1");
+
+  rnd = Math.floor(Math.random() * elements1.length);
+  elements1[rnd].setAttribute("opacity", "0");
 };
 
 setInterval(emme, 200);
